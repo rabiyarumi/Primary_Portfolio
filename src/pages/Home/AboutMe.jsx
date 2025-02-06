0a67ae767ef9c7394e437a60012079e324999d7c
rabiyarumi <rabiyaarumi@gmail.com>
2025-02-06 21:53:16 +0600

    modify about me section

--- src/pages/Home/AboutMe.jsx
+++ src/pages/Home/AboutMe.jsx
@@ -1,22 +1,36 @@
-import React from 'react';
+import image from "../../assets/portfolio_pic_bg_removed.png"
 
 const AboutMe = () => {
-    return (
-        <section className="py-12 px-6 sm:px-12 lg:px-24">
-        <div className="max-w-4xl mx-auto text-center">
-          <h2 className="text-4xl font-bold  mb-6">About Me</h2>
-          <p className="text-lg  leading-relaxed">
-          Hi, I’m [Your Name], a passionate learner and a budding software developer with an unquenchable curiosity for technology. My journey into programming began with a simple fascination—watching how lines of code could create something meaningful and impactful. I still remember the first time I built a small web page; it felt like unlocking a new world of possibilities. 
-          </p>
-          <p className="text-lg  leading-relaxed">
-            My journey into programming is simple yet exciting—I developed an interest in coding and decided to pursue it wholeheartedly. I enjoy implementing new and creative ideas in my projects, constantly exploring the latest technologies to expand my skills. 
-          </p>
-          <p className="text-lg  leading-relaxed mt-4">
-            Beyond programming, I have a passion for solving puzzles like Rubik's cubes and indulging in fashion design, where I get to express my creativity in unique ways. These hobbies help me stay inspired and bring a fresh perspective to my coding journey.
-          </p>
-        </div>
-      </section>
-    );
+  return (
+    <section className="py-12 px-6 sm:px-12 lg:px-24 ">
+      <h2 className="text-4xl font-bold  mb-10 text-center">About Me</h2>
+      <div className="md:flex gap-10">
+      <div className="md:w-1/2">
+        <img
+              src={image}
+              className=" h-full" />
+      </div>
+      <div className=" flex flex-col  justify-center md:w-1/2">
+        <p className="mb-3  ">
+          Hi, I’m Rabiya Rumi, a passionate learner and a budding software
+          developer with an unquenchable curiosity for technology. I thrive on transforming complex problems into intuitive user interfaces using React
+        </p>
+        <p className="mb-3">
+          My journey into programming is simple yet exciting—I developed an
+          interest in coding and decided to pursue it wholeheartedly. I enjoy
+          implementing new and creative ideas in my projects, constantly
+          exploring the latest technologies to expand my skills.
+        </p>
+        <p className="mb-3">
+          Beyond programming, I have a passion for solving puzzles like Rubik's
+          cubes and indulging in fashion design, where I get to express my
+          creativity in unique ways. These hobbies help me stay inspired and
+          bring a fresh perspective to my coding journey.
+        </p>
+      </div>
+      </div>
+    </section>
+  );
 };
 
 export default AboutMe;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,9 +9,9 @@ const Home = () => {
     return (
         <div>
             <Banner/>
+            <AboutMe/>
             <Skills/>
             <Projects/>
-            <AboutMe/>
             <SendEmail/>
         </div>
     );
