6484d23b58e91954ebd73be5ddc254b26f4bf74b
rabiyarumi <rabiyaarumi@gmail.com>
2025-02-07 10:59:04 +0600

    modify banner setion

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,31 +1,67 @@
-import React from 'react';
-import image from "../../assets/portfolio_pic_bg_removed.png"
-import { Link } from 'react-router-dom';
-import { FaFacebook, FaFacebookSquare, FaGithub, FaLinkedin } from 'react-icons/fa';
-import { FaSquareGithub } from 'react-icons/fa6';
+import React from "react";
+import image from "../../assets/portfolio_pic_bg_removed.png";
+import { Link } from "react-router-dom";
+import {
+  FaFacebook,
+  FaFacebookSquare,
+  FaGithub,
+  FaLinkedin,
+} from "react-icons/fa";
+import { FaSquareGithub } from "react-icons/fa6";
+import { Typewriter } from "react-simple-typewriter";
 
 const Banner = () => {
-    return (
-       <div className="hero  ">
-  <div className="hero-content flex-col md:flex-row-reverse">
-    <img
-      src={image}
-      className="w-1/2 " />
-    <div className='w-1/2'>
-      <h1 className="text-4xl font-bold">Hey! I am Rabiya Rumi </h1>
-      <p className="py-6">
-        I am a frontend Web Developer. I make buautifull Website with animation and eye pleased colour
-      </p>
-      <div className='flex gap-4 pb-4'>
-        <Link to={"https://www.facebook.com/rabeya.rumi.285936/?viewas=&should_open_composer=false&show_switched_toast=false&show_invite_to_follow=false&show_switched_tooltip=false&show_podcast_settings=false&show_community_review_changes=false&show_community_rollback=false&show_follower_visibility_disclosure=false&bypass_exit_warning=true"}><FaFacebookSquare size={"30"}/></Link>
-        <Link to={"https://www.facebook.com/rabeya.rumi.285936/?viewas=&should_open_composer=false&show_switched_toast=false&show_invite_to_follow=false&show_switched_tooltip=false&show_podcast_settings=false&show_community_review_changes=false&show_community_rollback=false&show_follower_visibility_disclosure=false&bypass_exit_warning=true"}><FaSquareGithub size={"30"}/></Link>
-        <Link to={"https://www.facebook.com/rabeya.rumi.285936/?viewas=&should_open_composer=false&show_switched_toast=false&show_invite_to_follow=false&show_switched_tooltip=false&show_podcast_settings=false&show_community_review_changes=false&show_community_rollback=false&show_follower_visibility_disclosure=false&bypass_exit_warning=true"}><FaLinkedin size={"30"}/></Link>
-      </div>
-      <a href="https://docs.google.com/document/d/1iyG9Ce7-W0zmcp_6V9Sues5Xp8Ib9je0HCMn3ake3UY/edit?tab=t.0" target="_blank" className="btn btn-primary">Download Resume</a>
+  return (
+    <div className=" my-10 px-16">
+      
+        <div className="flex flex-col   gap-2">
+          <p className="pl-1">Hey! I am</p>
+          <h1 className="text-5xl font-bold"> Rabiya Rumi </h1>
+          <h3 className="mt-2 text-2xl">
+            I'm a
+            <Typewriter
+              words={[
+                " Web Developer",
+                " MERN Stack Developer",
+                " JavaScript and React Enthusiast",
+              ]}
+              loop={0}
+              cursor
+              cursorStyle="_"
+              typeSpeed={50}
+              deleteSpeed={50}
+              delaySpeed={3000}
+            />
+          </h3>
+
+          <div className="flex gap-4 pb-4">
+            <Link
+              to={"https://www.facebook.com/rabeya.rumi.285936/"}
+              target="_blank"
+            >
+              <FaFacebookSquare size={"30"} />
+            </Link>
+            <Link to={"https://github.com/rabiyarumi"} target="_blank">
+              <FaSquareGithub size={"30"} />
+            </Link>
+            <Link
+              to={"https://www.linkedin.com/in/rabiya-rumi/"}
+              target="_blank"
+            >
+              <FaLinkedin size={"30"} />
+            </Link>
+          </div>
+          <a
+            href="https://drive.google.com/file/d/1Wa4HXbjJj_ko4W-GALeNEbuJre3OFGSp/view?usp=sharing"
+            target="_blank"
+            className="btn btn-primary w-fit"
+          >
+            Download Resume
+          </a>
+        </div>
+     
     </div>
-  </div>
-</div>
-    );
+  );
 };
 
 export default Banner;
